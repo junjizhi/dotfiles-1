9ec4d8e13a77a8b488228074d507e2aeaf2d0094
Junji Zhi <zhijjjj@gmail.com>
2017-10-22 01:03:28 -0400

    Change to use https://github.com/magicmonty git prompt

--- source/01_prompt.sh
+++ source/01_prompt.sh
@@ -1,18 +1,26 @@
-# Allow prompt to be restored to default.
-if [[ "${#__PROMPT_DEFAULT[@]}" == 0 ]]; then
-  __PROMPT_DEFAULT=("$PS1" "$PS2" "$PS3" "$PS4")
+# Rely on https://github.com/magicmonty/bash-git-prompt#user-content-installation instead.
+if [ ! -f ~/.bash-git-prompt/gitprompt.sh ]; then
+    echo "~/.bash-git-prompt/gitprompt.sh not found! Please git clone https://github.com/magicmontyto home directory."
+    return
 fi
 
-# The default prompt.
-function prompt_default() {
-  unset PROMPT_COMMAND
-  for i in {1..4}; do
-    eval "PS$i='${__PROMPT_DEFAULT[i-1]}'"
-  done
-}
+# Set config variables first
+GIT_PROMPT_ONLY_IN_REPO=0
 
-# An uber-simple prompt for demos / screenshots.
-function prompt_zero() {
-  prompt_default
-  PS1='$ '
-}
+GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
+
+# GIT_PROMPT_SHOW_UPSTREAM=1 # uncomment to show upstream tracking branch
+# GIT_PROMPT_SHOW_UNTRACKED_FILES=all # can be no, normal or all; determines counting of untracked files
+
+# GIT_PROMPT_SHOW_CHANGED_FILES_COUNT=0 # uncomment to avoid printing the number of changed files
+
+# GIT_PROMPT_STATUS_COMMAND=gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
+
+# GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+# GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
+
+# as last entry source the gitprompt script
+# GIT_PROMPT_THEME=Custom # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
+# GIT_PROMPT_THEME_FILE=~/.git-prompt-colors.sh
+GIT_PROMPT_THEME=Solarized # use theme optimized for solarized color scheme
+source ~/.bash-git-prompt/gitprompt.sh
